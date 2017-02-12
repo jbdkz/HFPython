95a210a86180d80c477e4b2718ffd38f6c9f767e
John Diczhazy <jbdkz100@gmail.com>
2017-02-12 01:40:45 +0000

    web log update

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,34 +1,30 @@
 from flask import Flask, render_template, request, redirect, escape
 from vsearch import search4letters
-import mysql.connector
+from DBcm import UseDatabase
 
 
 app = Flask(__name__)
 
+app.config['dbconfig'] = { 'host': '127.0.0.1',
+             'user': 'vsearch',
+             'password': 'vsearchpasswd',
+             'database': 'vsearchlogDB', }
+
 def log_request(req: 'flask_request', res: str) -> None:
-
-    dbconfig = { 'host': '127.0.0.1',
-                 'user': 'vsearch',
-                 'password': 'vsearchpasswd',
-                 'database': 'vsearchlogDB', }
-
-    
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    _SQL = """insert into log
+    """Log details of the web request and the resutls."""
+                   
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
               (phrase, letters, ip, browser_string, results)
               values
               (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form['phrase'],
+        cursor.execute(_SQL, (req.form['phrase'],
                           req.form['letters'],
                           req.remote_addr,
                           req.user_agent.browser,
                           res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
 
-    
+
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -45,8 +41,21 @@ def entry_page() -> 'html':
     return render_template('entry.html',the_title='Welcome to search4letters on the web!')
 
 @app.route('/viewlog')
-def view_the_log() -> str:
-    contents=[]
+def view_the_log() -> 'html':
+    """Display the contents of the log file as a HTML table."""
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+    titles = ('Phrase','Letters','Remote_addr','User_agent','Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
+
+
+
+
     with open('vsearch.log')as log:
         for line in log:
             contents.append([])
